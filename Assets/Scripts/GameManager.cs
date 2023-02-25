5a4e549e52bc267966bce48a4799bd9a140819ed
Zane Shaw <admin@triangle100.com>
2023-02-25 13:44:35 +1100

    Add game manager skeleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField] private GameSettings gameSettings;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct GameSettings {
+    public int preGameTime;
+    public int midGameTime;
+    public int endGameTime;
+}
