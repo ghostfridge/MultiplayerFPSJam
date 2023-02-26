f9b0f980263986b567ac0a03ff2afa9f7d188d78
Saba Shaw <sabashaw.home@gmail.com>
2023-02-26 15:39:41 +1100

    Remove network demo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,26 +24,6 @@ public class PlayerController : NetworkBehaviour {
     [SerializeField] private float groundDistance;
     [SerializeField] private float groundCheckOffset;
 
-    public override void OnNetworkSpawn() {
-        if (!IsServer && IsOwner) { //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
-            TestServerRpc(0, NetworkObjectId);
-        }
-    }
-
-    [ClientRpc]
-    void TestClientRpc(int value, ulong sourceNetworkObjectId) {
-        Debug.Log($"Client Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
-        if (IsOwner) { //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
-            TestServerRpc(value + 1, sourceNetworkObjectId);
-        }
-    }
-
-    [ServerRpc]
-    void TestServerRpc(int value, ulong sourceNetworkObjectId) {
-        Debug.Log($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
-        TestClientRpc(value, sourceNetworkObjectId);
-    }
-
     private void Awake() {
         controls = new InputMain();
     }
